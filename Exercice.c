4fc8a4d3c6a1abe178f97873e7d1b397dc7a3cdc
Ikaroc-info <leobertrand641@gmail.com>
2021-06-03 11:37:13 +0200

    hey

--- Exercice.c
+++ Exercice.c
@@ -22,15 +22,32 @@ void copy(char* t, char c[])
     }
 }
 
+int nmb_e(char*t)
+{
+    int res=0;
+    int nb_e=0;
+    while (*(t+res)!=0)
+    {
+        if (*(t+res)=='e')
+        {
+            nb_e++;
+        }
+        res++;
+    }
+    return nb_e; 
+}
+
 int main()
 {   // EXERCICE 1
     //char* c= "abcdefg";
     // printf("%d\n",mylen(c));
     // EXERCICE 2
-    char* c="abcdefg";
+    char* c="abedefg";
     char * t =(char*) malloc(8*sizeof(char));
     copy(t,c);
-    printf("%d\n",mylen(t));
+    printf("%d\n",nmb_e(t));
+    // EXERCICE 3
+
 
 }
 
