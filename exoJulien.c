c7ea57ec0e4a4e3ef49c8ebe53ac90843f7f392f
julienjeanjacquot@gmail.com <julienjeanjacquot@gmail.com>
2021-06-03 11:16:10 +0200

    add

--- exoJulien.c
+++ exoJulien.c
@@ -4,7 +4,22 @@ int main(){
 
 char* s = "absgyc";
 int l = len(s);
+
 printf("%d\n",l);
+
+
+char* s2 = "azertyuiop";
+char* s3 = malloc(10* sizeof(char));
+copi(s2,s3);
+
+while(*s3){
+	printf("%c",*s3);
+	++s3;
+
+
+}
+printf("\n");
+
 }
 
 
@@ -14,4 +29,15 @@ int len(char* s){
 		len ++;
 	}
 	return len;
+}
+
+void copi(char* s1,char* s2){
+	char* s = s2;
+	while(*s1){
+		*s2 = *s1;
+		++s1;
+		++s2;
+	}
+	s2 = s;
+
 }
